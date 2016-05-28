cf33aa8e4212f9ae3c2b9ecf8b9d75c7debb8b61
1egoman <rsg1egoman@gmail.com>
2016-05-28 09:07:24 -0400

    make all releases one final time

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-GOOS=linux GOARCH=386 go build ph && mv ph compiled/ph-linux-i386 && echo "linux i386"
-GOOS=linux GOARCH=amd64 go build ph && mv ph compiled/ph-linux-amd64 && echo "linux amd64"
+GOOS=linux GOARCH=386 go build ph && mv ph compiled/ph-linux-i686 && echo "linux i386"
+GOOS=linux GOARCH=amd64 go build ph && mv ph compiled/ph-linux-x86_64 && echo "linux amd64"
 
-GOOS=darwin GOARCH=386 go build ph && mv ph compiled/ph-darwin-i386 && echo "darwin i386"
-GOOS=darwin GOARCH=amd64 go build ph && mv ph compiled/ph-darwin-amd64 && echo "darwin amd64"
+GOOS=darwin GOARCH=386 go build ph && mv ph compiled/ph-darwin-i686 && echo "darwin i386"
+GOOS=darwin GOARCH=amd64 go build ph && mv ph compiled/ph-darwin-x86_64 && echo "darwin amd64"
